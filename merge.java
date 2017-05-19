51e8240fd6488c2a5497c4ef1a08e1f5b1416a39
musicclimax <kus00116@naver.com>
2017-05-19 17:46:03 +0900

    merge
    merge

--- merge.java
+++ merge.java
@@ -1,21 +1,20 @@
+package test;
 
 import java.util.LinkedList;
- 
+
 public class merge {	
-	public void copytoright(LinkedList<node> node1, LinkedList<node> node2){
+	public void copytoright(LinkedList<Node> node1, LinkedList<Node> node2, int selectionstartpos, int selectionendpos){
 		int length = node1.size();		
-		for(int i = 0; i < length; i++){
-			if((node1.get(i).getisDiff() == true)){
-				node2.get(i).setString(node1.get(i).getString());
-			}
+		for(int i = selectionstartpos; i < selectionendpos; i++){			
+			node2.get(i).setString(node1.get(i).getString());
+			node2.get(i).setIsAddedEnter(false);
 		}
 	}
-	public void copytoleft(LinkedList<node> node1, LinkedList<node> node2){
+	public void copytoleft(LinkedList<Node> node1, LinkedList<Node> node2, int selectionstartpos, int selectionendpos){
 		int length = node1.size();		
-		for(int i = 0; i < length; i++){
-			if((node2.get(i).getisDiff() == true)){
-				node1.get(i).setString(node2.get(i).getString());
-			}
+		for(int i = selectionstartpos; i < selectionendpos; i++){			
+			node1.get(i).setString(node2.get(i).getString());
+			node1.get(i).setIsAddedEnter(false);
 		}
 	}
  
