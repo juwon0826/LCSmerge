47f924e57fb4a63f86603bb4ef012287ec5b59ce
LeeKangJin <himnae5181@naver.com>
2017-05-22 10:03:12 +0900

    GUI resize못하게 막았음.
    음.. 혹시 더 좋은 함수가있으면 찾아봐야함

--- mergerGUI.java
+++ mergerGUI.java
@@ -76,6 +76,7 @@ public class mergerGUI extends JFrame {
   setBackground(Color.WHITE);
   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   setBounds(100, 100,1500,800);
+  setResizeable(false);
   contentPane = new JPanel();
   contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
   contentPane.setLayout(new BorderLayout(0, 0));
