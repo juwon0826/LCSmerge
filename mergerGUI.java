15fe3ecf7768314f585da4fa64602db2707b054b
LeeKangJin <himnae5181@naver.com>
2017-06-02 15:47:38 +0900

    gui 버튼 크기 및 비활성화 문제

--- mergerGUI.java
+++ mergerGUI.java
@@ -1,4 +1,4 @@
-package fefe;
+package merge;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -25,6 +25,7 @@ import javax.swing.JLabel;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.TextArea;
 
 import javax.swing.JEditorPane;
 import javax.swing.JTextArea;
@@ -47,15 +48,19 @@ import javax.swing.JComboBox;
 import javax.swing.BoxLayout;
 import javax.swing.Box;
 
+import java.util.LinkedList;
+
 public class mergerGUI extends JFrame {
- private JPanel contentPane;	
-        static boolean edit1 = false;
-	static boolean edit2 = false;
-	
+ private JPanel contentPane;
+ static boolean edit1 =true;
+ static boolean edit2 =true;
  /**
   * Launch the application.
   */
+ LinkedList<Node> L1, L2;
  public static void main(String[] args) {
+	 
+	
   EventQueue.invokeLater(new Runnable() {
    public void run() {
     try {
@@ -71,17 +76,14 @@ public class mergerGUI extends JFrame {
   * Create the frame.
   */
  public mergerGUI() {
-
-  //create frame	 
   setBackground(Color.WHITE);
   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  setBounds(100, 100,1500,800);
-  setResizable(false);
+  setBounds(100, 100,800,600);
   contentPane = new JPanel();
   contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
   contentPane.setLayout(new BorderLayout(0, 0));
   setContentPane(contentPane);
-  //create panel (up)
+  
   JPanel panel = new JPanel();
   contentPane.add(panel, BorderLayout.NORTH);
   panel.setLayout(new GridLayout(0, 3, 0, 0));
@@ -89,25 +91,30 @@ public class mergerGUI extends JFrame {
   JPanel panel_1 = new JPanel();
   panel.add(panel_1);
   panel_1.setLayout(new GridLayout(0, 3, 0, 0));
-  //create left-up button
+  
   JButton btnNewButton_5 = new JButton("load");
   panel_1.add(btnNewButton_5);
   
   btnNewButton_5.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-	      //Do it
+	      Load load = new Load();
+	      load.openPopup();
+	      load.loadFromFile();
+	      L1 = load.getList();
+	      
 	    }
 	});
 
   JButton btnNewButton = new JButton("edit");
   panel_1.add(btnNewButton);
-  
-
+ 
   JButton btnNewButton_3 = new JButton("save");
   panel_1.add(btnNewButton_3);
   btnNewButton_3.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-	      //Do it
+	      Save save = new Save(L1,"test1.txt");
+	      save.saveToFile();
+	      
 	    }
 	});
  
@@ -117,22 +124,28 @@ public class mergerGUI extends JFrame {
   JPanel panel_4 = new JPanel();
   panel.add(panel_4);
   panel_4.setLayout(new GridLayout(0, 3, 0, 0));
-  //create right -up button
+  
   JButton btnNewButton_1 = new JButton("load");
   panel_4.add(btnNewButton_1);
+  
+  btnNewButton_1.setEnabled(false); //¿¹½Ã change it
   btnNewButton_1.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-	      //Do it
+	    	 Load load = new Load();
+		      load.openPopup();
+		      load.loadFromFile();
+		      L2 = load.getList();
 	    }
 	});
   JButton btnNewButton_2 = new JButton("edit");
   panel_4.add(btnNewButton_2);
-
+ 
   JButton btnNewButton_4 = new JButton("save");
   panel_4.add(btnNewButton_4);
   btnNewButton_4.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-	      //Do it
+	    	Save save = new Save(L2,"test1.txt");
+		      save.saveToFile();
 	    }
 	});
   JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
@@ -143,22 +156,19 @@ public class mergerGUI extends JFrame {
   
   JPanel panel_3 = new JPanel();
   contentPane.add(panel_3, BorderLayout.CENTER);
-  panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+  panel_3.setLayout(new GridLayout(0, 3, 3, 3));
   
-   // create  left text area 
+
    JTextArea textArea_2 = new JTextArea(10,50);
     panel_3.add(textArea_2);
-    textArea_2.setEditable(false);
-   
+    //DO
+    
     btnNewButton.addActionListener(new ActionListener() {
-	    public void actionPerformed(ActionEvent e) {
-	    
-	    	edit1=!edit1;
-	    	textArea_2.setEditable(edit1);
-
-	    }
-	});
-    textArea_2.setLineWrap(true);
+  	    public void actionPerformed(ActionEvent e) {
+  	      edit2= !edit2;
+  	      textArea_2.setEditable(edit2);
+  	    }
+  	});
    JScrollPane scrollPane = new JScrollPane(textArea_2);
   
    scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -170,19 +180,20 @@ public class mergerGUI extends JFrame {
  
   
   JPanel panel_5 = new JPanel();
-  panel_5.setPreferredSize(new Dimension(14, 39));
+  panel_5.setPreferredSize(new Dimension(100, 1000));
   panel_3.add(panel_5);
   panel_5.setLayout(new BorderLayout(0, 0));
   
   JPanel panel_6 = new JPanel();
   panel_5.add(panel_6, BorderLayout.NORTH);
   panel_6.setLayout(new GridLayout(0, 1, 0, 0));
-  //create center button (compare & merge)
+  
   JButton btnNewButton_6 = new JButton("compare");
   panel_6.add(btnNewButton_6);
   btnNewButton_6.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-	      //Do it
+	      compare comp = new compare();
+	      comp.compareList(L1, L2);
 	    }
 	});
   JPanel panel_7 = new JPanel();
@@ -203,18 +214,18 @@ public class mergerGUI extends JFrame {
 	      //Do it
 	    }
 	});
-  // create right textarea
   JTextArea textArea = new JTextArea(10,50);
   panel_3.add(textArea);
-  textArea.setEditable(false);
   btnNewButton_2.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
-	    
-	    	edit2= !edit2;
-	    	textArea.setEditable(edit2);
+	      //Do it
+	    	edit1 = !edit1;
+	    	textArea.setEditable(edit1);
+	    	
+	    	
 	    }
 	});
-  textArea.setLineWrap(true);
+  
   JScrollPane scrollPane2 = new JScrollPane(textArea);
   scrollPane2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
   scrollPane2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -222,4 +233,3 @@ public class mergerGUI extends JFrame {
   panel_3.add(scrollPane2);
  }
 }
- 
